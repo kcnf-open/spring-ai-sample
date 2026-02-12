17968492c2e0de9088aaf58ff4f6be1fc28ce54f
kcnf <kcnf@qq.com>
2026-02-12 21:08:33 +0800

    init

--- spring-ai/spring-ai-deepseek1/src/main/java/com/kcnf/ai/controller/ChatController.java
+++ spring-ai/spring-ai-deepseek1/src/main/java/com/kcnf/ai/controller/ChatController.java
@@ -2,19 +2,22 @@ package com.kcnf.ai.controller;
 
 import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chat")
 public class ChatController {
 
     @Autowired
+    @Qualifier("deepSeekChatModel")
     private DeepSeekChatModel deepSeekChatModel;
 
-    @RequestMapping("/")
+    @GetMapping("/t")
     public String index(@RequestParam(value = "name", defaultValue = "World") String name){
-
         return deepSeekChatModel.call(name);
     }
 }
