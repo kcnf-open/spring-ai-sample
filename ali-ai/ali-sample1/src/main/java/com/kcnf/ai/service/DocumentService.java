ff452efe4ccb728daa7745aeb796bcc49559a0b6
kcnf <kcnf@qq.com>
2026-03-08 20:52:51 +0800

    init

--- ali-ai/ali-sample1/src/main/java/com/kcnf/ai/service/DocumentService.java
+++ ali-ai/ali-sample1/src/main/java/com/kcnf/ai/service/DocumentService.java
@@ -1,16 +1,11 @@
 package com.kcnf.ai.service;
 
-import org.springframework.ai.document.Document;
-import org.springframework.core.io.Resource;
-
-import java.util.List;
-
 public interface DocumentService {
 
-
-    List<Document> loadText(Resource resource, String fileName);
-
-
-    List<Document> doSearch(String question);
+//
+//    List<Document> loadText(Resource resource, String fileName);
+//
+//
+//    List<Document> doSearch(String question);
 
 }
