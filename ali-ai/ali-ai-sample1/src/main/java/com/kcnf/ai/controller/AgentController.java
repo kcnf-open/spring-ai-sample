0f5f7160ba1e8e9609243a264f1505687eb0dd7a
kcnf <kcnf@qq.com>
2026-03-11 21:11:43 +0800

    init

--- ali-ai/ali-ai-sample1/src/main/java/com/kcnf/ai/controller/AgentController.java
+++ ali-ai/ali-ai-sample1/src/main/java/com/kcnf/ai/controller/AgentController.java
@@ -4,11 +4,7 @@ import com.alibaba.cloud.ai.graph.RunnableConfig;
 import com.alibaba.cloud.ai.graph.action.InterruptionMetadata;
 import com.alibaba.cloud.ai.graph.agent.ReactAgent;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -28,8 +24,7 @@ public class AgentController {
     @GetMapping("/invoke")
     @ResponseBody
     public List<InterruptionMetadata.ToolFeedback> invoke(@RequestParam("query") String query,
-                                                          @RequestParam("threadId") String threadId
-    ) throws Exception {
+                                                          @RequestParam("threadId") String threadId) throws Exception {
         RunnableConfig runnableConfig = RunnableConfig.builder().threadId(threadId).build();
         InterruptionMetadata metadata = (InterruptionMetadata) reactAgent.invokeAndGetOutput(query, runnableConfig).orElseThrow();
         map.put(threadId, metadata);
