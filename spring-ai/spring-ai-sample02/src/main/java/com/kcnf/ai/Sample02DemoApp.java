a5663fdfa16b65e805bddcf6945dd210bf5194cb
kcnf <kcnf@qq.com>
2026-03-25 23:48:39 +0800

    init

--- spring-ai/spring-ai-sample02/src/main/java/com/kcnf/ai/Sample02DemoApp.java
+++ spring-ai/spring-ai-sample02/src/main/java/com/kcnf/ai/Sample02DemoApp.java
@@ -3,14 +3,12 @@ package com.kcnf.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations={"classpath:config.xml"})
-public class DemoApp {
+public class Sample02DemoApp {
     public static void main(String[] args) {
 
-        SpringApplication.run(DemoApp.class, args);
+        SpringApplication.run(Sample02DemoApp.class, args);
 
     }
 }
