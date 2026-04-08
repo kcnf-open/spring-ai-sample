9056799ba572c99970967d2f136542b2b9a44f5b
kcnf <kcnf@qq.com>
2026-04-08 21:41:45 +0800

    init

---
+++ spring-ai/spring-ai-sample10-mcp/spring-ai-sample10-mcp-server-stdio/src/main/java/com/kcnf/ai/config/McpConfig.java
@@ -0,0 +1,19 @@
+package com.kcnf.ai.config;
+
+import com.kcnf.ai.tool.MyServerTools;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class McpConfig {
+
+    @Bean
+    public ToolCallbackProvider myToolsProvider(MyServerTools myTools) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(myTools)
+                .build();
+    }
+
+}
